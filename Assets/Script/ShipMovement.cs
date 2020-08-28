63eeedb33a7c81f930a12f0417228d84f5d09d59
stwalsh4118 <seanwalsh4118@yahoo.com>
2020-08-27 23:28:21 -0500

    init

---
+++ Assets/Script/ShipMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    private float moveSpeed;
+    private bool moveRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 2f;
+        moveRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > 7f)
+        {
+            moveRight = false;
+        }
+        else if (transform.position.x < -7f)
+        {
+            moveRight = true;
+        }
+
+        if (moveRight)
+        {
+            transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
+        }
+    }
+}
