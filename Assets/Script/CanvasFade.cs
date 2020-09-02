e45473d54300f7c22516c0ee2a389c68b78efd33
stwalsh4118 <seanwalsh4118@yahoo.com>
2020-09-02 16:45:00 -0500

    scenes

---
+++ Assets/Script/CanvasFade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasFade : MonoBehaviour
+{
+
+    CanvasGroup cg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cg = GetComponent<CanvasGroup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+        if(worldPosition.x > 2)
+        {
+            cg.alpha = 1;
+        }
+        else
+        {
+            cg.alpha = 1;
+        }
+    }
+}
---
+++ Assets/Script/readInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class readInput : MonoBehaviour
+{
+
+    public GameObject inputField;
+    private float count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count += Time.deltaTime;
+        if (count >= 1)
+        {
+            string text = inputField.GetComponent<TMP_InputField>().text;
+            Debug.Log(text);
+            count = 0;
+            if(text == "load")
+            {
+                SceneManager.LoadScene("sceneexperiment");
+            }
+        }
+    }
+}
