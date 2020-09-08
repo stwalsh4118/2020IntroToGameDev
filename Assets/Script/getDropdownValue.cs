cfb1d19ca1754c3da7b41873b20a004d3cff8a9b
stwalsh4118 <seanwalsh4118@yahoo.com>
2020-09-07 20:26:42 -0500

    BossCommandFilesDropdown

---
+++ Assets/Script/getDropdownValue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class getDropdownValue : MonoBehaviour
+{
+    Dropdown D;
+    public string value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        D = GetComponent<Dropdown>();
+        setDropdown();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        value = D.options[D.value].text;
+    }
+
+    void setDropdown()
+    {
+        string[] fileN;
+        fileN = GetComponent<loadTextFiles>().fileNames;
+        foreach(string fileName in fileN)
+        {
+            Dropdown.OptionData m_NewData = new Dropdown.OptionData();
+            m_NewData.text = fileName;
+            D.options.Add(m_NewData);
+        }
+    }
+}
---
+++ Assets/Script/loadTextFiles.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class loadTextFiles : MonoBehaviour
+{
+    public string[] filePaths;
+    public string[] fileNames;
+    // Start is called before the first frame update
+    void Start()
+    {
+        filePaths = Directory.GetFiles(Application.dataPath + "/BossCommands", "*.txt");
+        fileNames = new string[filePaths.Length];
+        int i = 0;
+        foreach (string files in filePaths)
+        {
+            Debug.Log(Path.GetFileName(files));
+            fileNames[i] = Path.GetFileName(files);
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
