21d2eac350e0d2992c00caeca5b6e4abdf97d1d9
stwalsh4118 <seanwalsh4118@yahoo.com>
2020-09-19 19:37:49 -0500

    for branch

--- Assets/Script/Player/playerMovement.cs
+++ Assets/Script/Player/playerMovement.cs
@@ -8,12 +8,31 @@ public class playerMovement : MonoBehaviour
     Vector2 movement;
     public Animator animator;
 
+    public float cooldownTime = 5f;
+    public float nextFireTime = 0f;
+
+    void loseSpeed()
+    {
+        moveSpeed -= 2f;
+    }
+
     // Update is called once per frame. Don't use it for physics-related functions. 
     // Use for registering input.
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Time.time > nextFireTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                moveSpeed += 2f;
+                Debug.Log("Button is pressed");
+                nextFireTime = Time.time + cooldownTime;
+                Invoke("loseSpeed", cooldownTime);
+            }
+        }
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
